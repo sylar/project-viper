fa19f077945d5050ee0ed61bda0d4871d598260f
Andrei Constantinescu <andrei@constantinescu.io>
2016-11-24 18:28:35 +0000

    Hook in reactron for easier debugging.

--- client/reactotron.config.js
+++ client/reactotron.config.js
@@ -3,12 +3,14 @@ if (process.env.NODE_ENV !== 'production') {
   const { trackGlobalErrors } = require('reactotron-react-js')
   const { reactotronRedux } = require('reactotron-redux')
   const apisaucePlugin = require('reactotron-apisauce')
+  const sagaPlugin = require('reactotron-redux-saga')
 
   Reactotron
     .configure()
-    .use(trackGlobalErrors())
-    .use(reactotronRedux())
     .use(apisaucePlugin())
+    .use(reactotronRedux())
+    .use(sagaPlugin())
+    .use(trackGlobalErrors())
     .connect()
 
     console.tron = Reactotron
