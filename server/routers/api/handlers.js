a71eb8b756808aa27da5c8c2a215f7434ca6c74a
Andrei Constantinescu <andrei@constantinescu.io>
2017-04-16 14:00:00 +0100

    Es6 all the way.

--- server/routers/api/handlers.js
+++ server/routers/api/handlers.js
@@ -1,34 +1,36 @@
-var request = require("request"),
-cheerio = require("cheerio"),
-randomstring = require('randomstring')
-url = 'https://watch5s.is/movie/the-sorcerers-apprentice/watch/',
-md5 = require('md5');
+const request = require('request')
+const cheerio = require('cheerio')
+const randomstring = require('randomstring')
+const url = 'https://watch5s.is/movie/the-sorcerers-apprentice/watch/'
+const md5 = require('md5')
 
-
-const getMovie = () => new Promise(resolve => request({
+const getMovie = () => new Promise((resolve, reject) => request({
   url
-}, function (error, response, body) {
-  if (!error) {
-    var $ = cheerio.load(body),
-    playurl = $('.les-content a').attr('href'),
-    id = $('input[name="episodeID"]').attr('value'),
-    token = randomstring.generate(8),
-    key = md5('!@#$%^&*(' + id + token),
-    value = md5(token + url + id),
-    cookieString=`${key}=${value}`
-
-      request({
-        url: `https://play.watch5s.is/grabber-api/episode/${id}?token=${token}`,
-        headers: {
-            cookie: cookieString,
-            referer: url
-         }
-      },
-      (err, res) => resolve(JSON.parse(res.body)))
-
-  } else {
-    console.log("We’ve encountered an error: " + error);
+}, (err, response, body) => {
+  if (err) {
+    return reject(`err:${err}`)
   }
+  const $ = cheerio.load(body)
+  const playurl = $('.les-content a').attr('href')
+  const id = $('input[name="episodeID"]').attr('value')
+  const token = randomstring.generate(8)
+  const key = md5('!@#$%^&*(' + id + token)
+  const value = md5(token + url + id)
+  const cookieString = `${key}=${value}`
+
+  request({
+    url: `https://play.watch5s.is/grabber-api/episode/${id}?token=${token}`,
+    headers: {
+      cookie: cookieString,
+      referer: url
+    }
+  },
+  (err, res) => {
+    if (err) {
+      return reject(`err:${err}`)
+    }
+    resolve(JSON.parse(res.body))
+  })
 }
 ))
 
